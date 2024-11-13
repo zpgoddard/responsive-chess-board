c5ae95740a37bdf224d43e837cc3f14faec845df
Zac-Goddard-Codices <zpgoddard@gmail.com>
2024-11-13 19:39:33 +0000

    build uploaded

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import { intializeApp } from 'firebase/app';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDW25Jf2ESeuNz1neY7v0W9F5JCAyPUlrI",
+  authDomain: "responsive-chess-board.firebaseapp.com",
+  projectId: "responsive-chess-board",
+  storageBucket: "responsive-chess-board.firebasestorage.app",
+  messagingSenderId: "730120650854",
+  appId: "1:730120650854:web:42635a697c428015c4eda7",
+  measurementId: "G-RH27ZB0SBZ"
+};
+
+intializeApp(firebaseConfig);
--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/responsive-chess-board/',
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
