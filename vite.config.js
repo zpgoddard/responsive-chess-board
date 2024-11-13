30d44d7390ba719ccc701428fa9679f5e48b9a4e
Zac-Goddard-Codices <zpgoddard@gmail.com>
2024-11-13 18:40:12 +0000

    base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/responsive-chess-board/',
   plugins: [
     vue(),
     vueDevTools(),
